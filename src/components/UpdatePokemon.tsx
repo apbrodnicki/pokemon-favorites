d11da632eacad1e9b2dfbc655155648eeb6e65dd
Alex Brodnicki <apbrodnicki@gmail.com>
2024-03-18 00:53:39 -0500

    Support adding/removing multiple pokemon at a time.

--- src/components/UpdatePokemon.tsx
+++ src/components/UpdatePokemon.tsx
@@ -1,4 +1,4 @@
-import { Autocomplete, Button, Grid, Paper, TextField } from '@mui/material';
+import { Autocomplete, Button, Grid, ListItem, Paper, TextField } from '@mui/material';
 import { useFetchAllPokemon } from 'api/useFetchAllPokemon';
 import { PokemonListContext } from 'contexts/PokemonListContext';
 import { SnackbarContext } from 'contexts/SnackbarContext';
@@ -8,7 +8,7 @@ export const UpdatePokemon = (): React.JSX.Element => {
 	const { setSnackbarOpen, setSnackbarMessage, setSnackbarColor } = useContext(SnackbarContext);
 	const { pokemonList, setPokemonList } = useContext(PokemonListContext);
 
-	const [pokemonInput, setPokemonInput] = useState<string>('');
+	const [pokemonInput, setPokemonInput] = useState<string[]>([]);
 
 	const allPokemon = useFetchAllPokemon();
 
@@ -16,40 +16,46 @@ export const UpdatePokemon = (): React.JSX.Element => {
 		localStorage.setItem('pokemon-favorites_list', JSON.stringify(pokemonList));
 	}, [pokemonList]);
 
-	const onAutocompleteChange = (value: string | null): void => {
-		if (value !== null) {
-			setPokemonInput(value);
-		}
+	const onAutocompleteChange = (value: string[]): void => {
+		setPokemonInput(value);
 	};
 
 	const UpdatePokemon = (action: 'add' | 'remove'): void => {
-		if (pokemonInput === '') {
+		if (pokemonInput.length < 1) {
 			setSnackbarMessage('Error: Input value is empty.');
 			setSnackbarColor('error');
 			setSnackbarOpen(true);
-			setPokemonInput('');
+
+			setPokemonInput([]);
+
 			return;
 		}
 
 		if (action === 'add') {
-			if (!pokemonList.includes(pokemonInput)) {
-				setSnackbarMessage('Success: Pokémon added.');
-				setSnackbarColor('success');
-				setSnackbarOpen(true);
-				setPokemonList([...pokemonList, pokemonInput]);
-			} else {
-				setSnackbarMessage('Error: Pokémon already exists in list.');
-				setSnackbarColor('error');
-				setSnackbarOpen(true);
+			for (const name of pokemonInput) {
+				if (pokemonList.includes(name)) {
+					setSnackbarMessage(`Error: ${name} has already been added.`);
+					setSnackbarColor('error');
+					setSnackbarOpen(true);
+
+					return;
+				}
 			}
+
+			setPokemonList([...pokemonList, ...pokemonInput]);
+
+			setSnackbarMessage('Success: Pokémon added.');
+			setSnackbarColor('success');
+			setSnackbarOpen(true);
 		} else if (action === 'remove') {
+			setPokemonList(pokemonList.filter((name) => !pokemonInput.includes(name)));
+
 			setSnackbarMessage('Success: Pokémon removed.');
 			setSnackbarColor('success');
 			setSnackbarOpen(true);
-			setPokemonList(pokemonList.filter((pokemon) => pokemon !== pokemonInput));
 		}
 
-		setPokemonInput('');
+		setPokemonInput([]);
 	};
 
 	return (
@@ -58,8 +64,21 @@ export const UpdatePokemon = (): React.JSX.Element => {
 				<Grid item xs={5} mx={3}>
 					<Autocomplete
 						options={allPokemon}
+						multiple
+						disableCloseOnSelect
 						isOptionEqualToValue={(option, value) => option === value}
 						onChange={(_, value) => { onAutocompleteChange(value); }}
+						renderOption={(props, option, { selected }) => (
+							<ListItem {...props}>
+								{/* <Checkbox
+									icon={<CheckBoxOutlineBlankIcon />}
+									checkedIcon={<CheckBoxIcon />}
+									style={{ marginRight: 8 }}
+									checked={selected}
+								/> */}
+								{option}
+							</ListItem>
+						)}
 						renderInput={(params) => (
 							<>
 								<TextField
@@ -77,8 +96,21 @@ export const UpdatePokemon = (): React.JSX.Element => {
 				<Grid item xs={5} mx={3}>
 					<Autocomplete
 						options={pokemonList}
+						multiple
+						disableCloseOnSelect
 						isOptionEqualToValue={(option, value) => option === value}
 						onChange={(_, value) => { onAutocompleteChange(value); }}
+						renderOption={(props, option, { selected }) => (
+							<ListItem {...props}>
+								{/* <Checkbox
+									icon={<CheckBoxOutlineBlankIcon />}
+									checkedIcon={<CheckBoxIcon />}
+									style={{ marginRight: 8 }}
+									checked={selected}
+								/> */}
+								{option}
+							</ListItem>
+						)}
 						renderInput={(params) => (
 							<>
 								<TextField
